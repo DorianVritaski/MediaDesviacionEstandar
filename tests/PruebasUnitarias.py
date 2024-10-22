7f76139c8782ebd6de3211f7650427e02bb6d436
DorianVritaski <72947488@continetal.edu.pe>
2024-10-22 02:39:17 -0500

    Refactorizacion del codigo: inplementacion de Extraccion de metodo e implementacion de inline

--- tests/PruebasUnitarias.py
+++ tests/PruebasUnitarias.py
@@ -2,99 +2,88 @@ import unittest
 from src.logica.Operaciones import Operaciones, NoSePuedeCalcular
 
 class Pruebas(unittest.TestCase):
-    # Pruebas para calcular la media
+
+    def crear_operacion(self, elementos):
+        return Operaciones(elementos)
+
     def test_media_listaVacia_lanzaExcepcion(self):
-        elementos = []
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([])
         with self.assertRaises(NoSePuedeCalcular):
             operacion.calcularMedia()
 
     def test_media_unElemento_retornaElemento(self):
-        elementos = [3.5]
         resultadoEsperado = 3.5
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([3.5])
         resultadoActual = operacion.calcularMedia()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_media_dosElementos(self):
-        elementos = [3, 7]
         resultadoEsperado = 5
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([3, 7])
         resultadoActual = operacion.calcularMedia()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_media_elementosPositivos(self):
-        elementos = [1, 2, 3, 4, 5]
         resultadoEsperado = 3
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([1, 2, 3, 4, 5])
         resultadoActual = operacion.calcularMedia()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_media_todosCeros(self):
-        elementos = [0, 0, 0, 0, 0]
         resultadoEsperado = 0
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([0, 0, 0, 0, 0])
         resultadoActual = operacion.calcularMedia()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_media_elementosPositivosYNegativos(self):
-        elementos = [-1, 2, -3, 4]
         resultadoEsperado = 0.5
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([-1, 2, -3, 4])
         resultadoActual = operacion.calcularMedia()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_media_elementosNoNumericos_lanzaExcepcion(self):
-        elementos = [1, 'a', 3]
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([1, 'a', 3])
         with self.assertRaises(TypeError):
             operacion.calcularMedia()
 
     # Pruebas para calcular la desviación estándar
     def test_desviacion_listaVacia_lanzaExcepcion(self):
-        elementos = []
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([])
         with self.assertRaises(NoSePuedeCalcular):
             operacion.calcularDesviacionEstandar()
 
     def test_desviacion_unElemento(self):
-        elementos = [5]
         resultadoEsperado = 0
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([5])
         resultadoActual = operacion.calcularDesviacionEstandar()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_desviacion_dosElementos(self):
-        elementos = [2, 4]
         resultadoEsperado = 1
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([2, 4])
         resultadoActual = operacion.calcularDesviacionEstandar()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_desviacion_elementosPositivos(self):
-        elementos = [2, 4, 4, 4, 5, 5, 7, 9]
         resultadoEsperado = 2
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([2, 4, 4, 4, 5, 5, 7, 9])
         resultadoActual = operacion.calcularDesviacionEstandar()
         self.assertAlmostEqual(resultadoEsperado, resultadoActual, places=2)
 
     def test_desviacion_todosCeros(self):
-        elementos = [0, 0, 0, 0, 0]
         resultadoEsperado = 0
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([0, 0, 0, 0, 0])
         resultadoActual = operacion.calcularDesviacionEstandar()
         self.assertEqual(resultadoEsperado, resultadoActual)
 
     def test_desviacion_elementosPositivosYNegativos(self):
-        elementos = [-2, -1, 0, 1, 2]
         resultadoEsperado = 1.4142
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([-2, -1, 0, 1, 2])
         resultadoActual = operacion.calcularDesviacionEstandar()
         self.assertAlmostEqual(resultadoEsperado, resultadoActual, places=2)
 
     def test_desviacion_elementosNoNumericos_lanzaExcepcion(self):
-        elementos = [1, 'a', 3]
-        operacion = Operaciones(elementos)
+        operacion = self.crear_operacion([1, 'a', 3])
         with self.assertRaises(TypeError):
             operacion.calcularDesviacionEstandar()
 
